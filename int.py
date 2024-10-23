f787568eff53402410a2cd07ad33f2c2edb0bf96
NMFlight <nmflight@DESKTOP-0K5AA2A.attlocal.net>
2024-10-22 19:30:43 -0700

    re-write to use stack for loops

--- int.py
+++ int.py
@@ -1,6 +1,15 @@
-## Run with hexdump at end: python3 int.py <file> && hexdump -e'"%07.8_ad  " 8/1 "%03u " "  " 8/1 "%03u " "  |"' -e'16/1  "%_p"  "|\n"' -e'"%07.8_Ad\n"' mem.dat
+## Run with hexdump at end: python3 int_copy.py <file> && hexdump -e'"%07.8_ad  " 8/1 "%03u " "  " 8/1 "%03u " "  |"' -e'16/1  "%_p"  "|\n"' -e'"%07.8_Ad\n"' mem.dat
 
 from sys import argv, stdout, stdin
+
+def find(string, char):
+    return [i for i, ltr in enumerate(string) if ltr == char]
+
+def peek(stack):
+    if stack:
+        return stack[-1]
+    else:
+        return None
 
 def update_mem(operation, num):
     global mem
@@ -16,17 +25,11 @@ def update_mem(operation, num):
         except ValueError:
             mem[pointer] = 255 - (num - 1) # Overflow
 
-def iterate(loop):
-    while mem[pointer] != 0:
-        for i, cmd in enumerate(loop):
-            check_char(cmd, i)
 
-
-def check_char(cmd, i):
-    # print(cmd)
+def check_char(cmd):
     global cleaned_code
     global pointer
-    global loops
+    global loop
     if cmd in '-+':
         update_mem(cmd, 1)
     elif cmd == '>':
@@ -35,20 +38,12 @@ def check_char(cmd, i):
     elif cmd == '<':
         if pointer != 0:
             pointer -= 1
-    elif cmd == '[':
-        q = 1
-        loop = ''
-        while cleaned_code[i + q] != ']':
-            loop_indexes.append(i + q)
-            loop = loop + cleaned_code[i + q]
-            q += 1
-        iterate(loop)
     elif cmd == '.':
         stdout.write(chr(mem[pointer]))
-        # with open('mem.dat', 'wb+') as dat:
-        #     dat.write(mem)
-    elif cmd == ',':
-        update_mem('+', ord(stdin.read(1)))
+        stdout.flush()
+    return None
+    # elif cmd == ',':
+    #     update_mem('+', ord(stdin.read(1)))    
 
 
 file = argv[1]
@@ -67,9 +62,46 @@ cleaned_code = list(cleaned_code)
 mem = bytearray(30000)
 pointer = 0
 
+stack = []
+index = 0
 
-for i, cmd in enumerate(cleaned_code):
-    check_char(cmd, i)
+while index != len(cleaned_code):
+    char = cleaned_code[index]
+    if char == '[':
+        if mem[pointer] == 0:
+            bstack = 1
+            i = index + 1
+            while bstack != 0:
+                if cleaned_code[i] == '[':
+                    bstack += 1
+                elif cleaned_code[i] == ']':
+                    bstack -= 1
+                i += 1
+            index = i
+        else:
+            if peek(stack) != index:
+                stack.append(index)
+            index += 1
+
+
+    elif char == ']':
+        if mem[pointer] == 0:
+            stack.pop()
+            index += 1
+        else:
+            index = peek(stack)
+        
+
+    else:
+        check_char(char)
+        index += 1
+
+
+
+
+
+with open('mem.dat', 'wb+') as dat:
+    dat.write(mem)
 
 
 stdout.write('\n\n')
